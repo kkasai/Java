74da57e23c1a27e851ce92650aa9155aa565ea6c
kasai keita <kasai@example.local>
2016-01-14 22:56:50 +0900

    画面のサンプルを作成しました。

---
+++ AndroidSample/app/src/main/java/com/example/itcollege/androidsample/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.itcollege.androidsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button submitButton = (Button) findViewById(R.id.submit);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = (EditText) findViewById(R.id.editText);
+                TextView textView = new TextView();
+                textView.setText(editText.getText());
+                LinearLayout linearLayout = (LinearLayout) findViewById(R.id.content);
+                linearLayout.addView(textView);
+
+            }
+        });
+    }
+
+}
